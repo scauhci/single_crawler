f2b016294ee6a62e6159279d89e4cc7430b29057
sq-zhou <zhousq109@163.com>
2017-01-19 01:09:54 +0800

    style:为filter.js添加注释,使其易读

--- apps/sq-zhou/app/filter.js
+++ apps/sq-zhou/app/filter.js
@@ -12,7 +12,22 @@ function filter(html){
     var $=cheerio.load(html);
     var title=$(distinguish.header);
     $(title).each(function(index,elem){
-        detail_message.title.push(elem.firstChild.data);
+        /*
+         *关于代码为什么用这样的方式可以从$title是一个多元素集合节点
+         * 其中$title中的一个节点含有如下的一些结构:
+         * attribs:
+         { class: 'article-title',
+         href: '/2016/08/04/javascript/pattern/singleton/' },
+         children:
+         [ { data: 'Javascript 设计模式之单例模式',
+         type: 'text',
+         next: null,
+         prev: null,
+         * parent: [Circular] } ]}；
+
+         *从结构出发，可以得到如下方法（可以自行console（elem）来查看节点的信息）
+         */
+        detail_message.title.push(elem.children[0].data);
         detail_message.detailUrl.push(url+elem.attribs.href);
     });
     return detail_message;
